d6e09d2217fcac8cf6b060a35a2a041f54c19f0a
saichakra-dev <gundasaichakra316@gmail.com>
2024-08-09 18:41:53 +0530

    second-command

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FastOption from "./components/FastOption";
 import LandingPage from "./components/LandingPage";
 
 const App = () => {
@@ -8,7 +7,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/fast" element={<FastOption />} />
       </Routes>
     </Router>
   );
